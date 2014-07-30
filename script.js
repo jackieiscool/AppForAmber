25a74e564bbe7e728ca6cc8ec311e7af3f645d76
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-07-29 22:19:06 -0700

    Some more style

--- script.js
+++ script.js
@@ -2,12 +2,55 @@ $(document).ready(function() {
   addToHomescreen();
 
   function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(44.9833, -93.2667),
-      zoom: 8
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
+    var myPosition = {};
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      alert('geolocation not supported');
+    }
+
+    function success(position) {
+      myPosition.latitude =  position.coords.latitude;
+      myPosition.longitude = position.coords.longitude;
+      console.log(myPosition);
+      makeMap();
+    }
+
+    function error(msg) {
+      myPosition.latitude = 44.9833;
+      myPosition.longitude = -93.2667;
+      makeMap();
+    }
+
+    function makeMap() {
+      console.log(myPosition);
+      var LatLng = new google.maps.LatLng(myPosition.latitude, myPosition.longitude)
+
+      var mapOptions = {
+        center: LatLng,
+        zoom: 18
+      };
+      var map = new google.maps.Map(document.getElementById("map-canvas"),
+          mapOptions);
+
+      var contentString = "<div class='popup'>Things</div>"
+
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+      var marker = new google.maps.Marker({
+        position: LatLng,
+        map: map,
+        icon: 'Cab.png',
+        title: 'Start'
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+      });
+    } 
   }
+
   google.maps.event.addDomListener(window, 'load', initialize);
 });
